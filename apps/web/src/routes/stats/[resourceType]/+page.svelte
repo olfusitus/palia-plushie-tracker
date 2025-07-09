e61e2ca8c6069143229299e1d81be47a629eb4fe
Flo Maier <florian@maierflo.de>
2025-07-09 13:15:10 +0200

    fix: Reversed data order for statistics

--- apps/web/src/routes/stats/[resourceType]/+page.svelte
+++ apps/web/src/routes/stats/[resourceType]/+page.svelte
@@ -20,7 +20,7 @@
 	});
 
 	$: entries = ($resourceStore[resourceType] || []).sort(
-		(a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
+		(a, b) => new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime()
 	);
 
 	$: {
